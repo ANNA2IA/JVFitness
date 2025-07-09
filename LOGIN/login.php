d111a18e403afdc79fcbd9eec7ec24c64efd92ab
Alejo061117 <ale.gutimejia0811@gmail.com>
2025-07-09 15:58:46 -0600

    prueba
    ...

--- LOGIN/login.php
+++ LOGIN/login.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header("Location: ../MENU/menu.php");
             exit();
         } else {
-            $mensaje = "Usuario o contraseña incorrectos.<br>";
+            $mensaje = "Usuario o contraseña malos.<br>";
         }
 
         $stmt->close();
